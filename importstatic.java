7329357f6eba79d2d70e7e44db96da8ca13fb3a2
AATMAN(ATOMIK) <94962387+AATMAN-ATOMIK@users.noreply.github.com>
2022-02-17 09:10:52 +0530

    Add files via upload

---
+++ importstatic.java
@@ -0,0 +1,7 @@
+import static java.lang.System.*;
+public class importstatic {
+    public static void main(String[] args) {
+        out.println("This is demo of static import ^_^");
+        // out.println("Hello");
+    }
+}
