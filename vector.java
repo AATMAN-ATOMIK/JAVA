fa696f5a2f64176498ac61f5b1962d5f30b826c1
AATMAN(ATOMIK) <94962387+AATMAN-ATOMIK@users.noreply.github.com>
2022-02-17 09:10:38 +0530

    Add files via upload

---
+++ vector.java
@@ -0,0 +1,16 @@
+import java.util.Vector;
+import java.util.ArrayList;
+public class vector {
+    public static void main(String[] args) {
+        Vector v = new Vector();
+        v.add("Aatman");
+        v.add("45");
+        v.add("50");
+        v.add("54");
+        v.add("32");
+       
+        System.out.println(v);
+
+        
+    }
+}
