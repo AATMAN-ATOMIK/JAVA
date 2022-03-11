fc298b8e0d9fa83356d89db0b358e99181c0cd08
AATMAN(ATOMIK) <94962387+AATMAN-ATOMIK@users.noreply.github.com>
2022-03-11 08:42:58 +0530

    Add files via upload

---
+++ reversestring.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class reversestring{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System. out. println("Enter string to reverse:");
+        String str = sc.nextLine();
+        StringBuilder sb = new StringBuilder(str);
+        System. out. println("Reversed string is:"); System.out.println(sb.reverse().toString());
+        sc.close();
+    } 
+}
